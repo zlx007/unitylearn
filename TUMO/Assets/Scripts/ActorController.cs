73b474e5e723bf3f2320dc49d07c6853ea66d309
zlx007 <1257826202@qq.com>
2021-11-30 18:33:01 +0800

    player move

--- TUMO/Assets/Scripts/ActorController.cs
+++ TUMO/Assets/Scripts/ActorController.cs
@@ -6,17 +6,97 @@ public class ActorController : MonoBehaviour
 {
     public GameObject model;
     public PlayerInput pi;
+    public  float walkSpeed = 1.5f;
+    public float runMultiplier = 2.0f;
+    public float jumpVelocity = 5.0f;
+    public float rollVelocity = 1.0f;
+    //public float jabVelocity = 3.0f;
 
     [SerializeField]
     private Animator anim;
+    private Rigidbody rigid;
+    private Vector3 planarVec;
+    private Vector3 thrustVec;//冲量
+
+
+    private bool lockPlanar = false;
+    //private float rotateSpeed = 2f;
     void Awake()
     {
         pi = GetComponent<PlayerInput>();
         anim = model.GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
     void Update()
     {
-        anim.SetFloat("forward",pi.Dmag);
-        model.transform.forward = pi.Dvec;
+        //速度暴增用插值
+        float targetRunMulti = ((pi.run) ? 2.0f : 1.0f);
+        anim.SetFloat("forward",pi.Dmag*Mathf.Lerp(anim.GetFloat("forward"),targetRunMulti,0.5f) );
+        if (rigid.velocity.magnitude>1.0f) {
+            anim.SetTrigger("roll");
+        }
+
+        if (pi.jump==true) {
+            anim.SetTrigger("jump");
+        }
+        
+        if (pi.Dmag>0.1f) {
+            model.transform.forward = Vector3.Slerp(model.transform.forward,pi.Dvec,0.3f);
+            
+        }
+        if (lockPlanar==false)
+        {         
+            planarVec = pi.Dmag * model.transform.forward * walkSpeed * ((pi.run) ? runMultiplier : 1.0f);
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        //rigid.position += planarVec * Time.fixedDeltaTime;
+        rigid.velocity =new Vector3(planarVec.x,rigid.velocity.y,planarVec.z)+thrustVec;
+        thrustVec = Vector3.zero;
+    }
+///
+///message processing block
+///
+    public void OnJumpEnter() {
+        thrustVec = new Vector3(0, jumpVelocity, 0);
+        pi.inputEnable = false;
+        lockPlanar = true;
+        
+    }
+    //public void OnJumpExit()
+    //{
+    //    pi.inputEnable = true;
+    //    lockPlanar = false;
+    //}
+    public void IsGround() {
+   
+        anim.SetBool("isGround",true);
+    }
+    public void IsNotGround() {
+        
+        anim.SetBool("isGround", false);
+    }
+    public void OnGroundEnter() {
+        pi.inputEnable = true;
+        lockPlanar = false;
+    }
+    public void OnFallEnter() {
+        pi.inputEnable = false;
+        lockPlanar = true;
+    }
+    public void OnRollEnter() {
+        thrustVec = new Vector3(0, rollVelocity, 0);
+        pi.inputEnable = false;
+        lockPlanar = true;
+    }
+    public void OnJabEnter() {
+
+        pi.inputEnable = false;
+        lockPlanar = true;
+    }
+    public void OnJabUpdate() {
+        thrustVec = model.transform.forward * anim.GetFloat("jabVelocity") ;
     }
 }
--- TUMO/Assets/Scripts/PlayerInput.cs
+++ TUMO/Assets/Scripts/PlayerInput.cs
@@ -4,35 +4,70 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    [Header("=====key settings=====")]
     public string keyUp;
     public string keyDown;
     public string keyLeft;
     public string keyRight;
     
+    public string keyA;
+    public string keyB;
+    public string keyC;
+    public string keyD;
+
+    [Header("=====Output singals=====")]
     public float Dup;
     public float Dright;
     public float Dmag;
     public Vector3 Dvec;
 
+    public bool run;
+    public bool jump;
+    private bool lastJump;
 
+    [Header("=====other======")]
     public bool inputEnable = true;
 
     private float TargetDup;
     private float TargetDright;
     private float velocityDup;
     private float velocityDright;
+
     void Update()
     {
         TargetDup = (Input.GetKey(keyUp)?1.0f:0) - (Input.GetKey(keyDown)?1.0f:0); 
+        //TargetDup = Input.GetAxis("Vertical");
         TargetDright= (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
+        //TargetDright = Input.GetAxis("Horizontal");
         if (inputEnable==false) {
             TargetDup = 0;
             TargetDright = 0;
         }
         Dup = Mathf.SmoothDamp(Dup,TargetDup,ref velocityDup,0.1f);
         Dright = Mathf.SmoothDamp(Dright,TargetDright,ref velocityDright,0.1f);
+        Vector2 square = new Vector2(Dright, Dup);
+        Dright=SquareToCircle(square).x;
+        Dup = SquareToCircle(square).y;
         Dmag = Mathf.Sqrt((Dup * Dup) + (Dright * Dright));
-        Dvec = Dright * transform.right + Dup * transform.forward;
+        //Fixme:转向后的上下左右还是原来的
+        Dvec = Dright *transform.right + Dup *transform.forward;
+        run = Input.GetKey(keyA);
+        bool newJump = Input.GetKey(keyB);
+        if (newJump!=lastJump&&lastJump==true) {
+            jump = true;
+            //print("!!!!");
+        }
+        else {
+            jump = false;
+        }
+        lastJump = newJump;
+
+    }
+    Vector2 SquareToCircle(Vector2 square) {//椭圆映射
+        Vector2 circle;
+        circle.x = square.x * Mathf.Sqrt(1-(square.y*square.y)/2);
+        circle.y = square.y * Mathf.Sqrt(1-(square.x*square.x)/2);
+        return circle;
     }
 
 
